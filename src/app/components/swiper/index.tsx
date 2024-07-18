6f0b9d19e8a40046d99648539198a7c3940204fe
Desmond <ifeco133@gmail.com>
2024-07-18 23:36:44 +0100

    images

--- src/app/components/swiper/index.tsx
+++ src/app/components/swiper/index.tsx
@@ -1,31 +1,43 @@
 "use client";
-import React, { TouchEvent, useRef, useState } from "react";
+import React, {
+  TouchEvent,
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 import "./swiper-styles.css";
+import image1 from "@/public/img1.jpg";
+import image2 from "@/public/img2.jpg";
+import image3 from "@/public/img3.avif";
+import image4 from "@/public/img4.avif";
+import image5 from "@/public/img5.avif";
+import image6 from "@/public/img6.avif";
+import image7 from "@/public/img7.avif";
+import image8 from "@/public/img8.jpg";
 
 const Swiper = ({ images }: { images: Array<string> }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const previousIndex = useRef<number>(0);
   const [isTransitioning, setIsTransitioning] = useState(false);
   const visibleImagesCount = 5;
   const touchStartX = useRef(0);
   const touchEndX = useRef(0);
 
+  useEffect(() => {
+    previousIndex.current = currentIndex;
+  }, [currentIndex]);
+
   const handleNext = () => {
     if (currentIndex < images.length - visibleImagesCount) {
-      setIsTransitioning(true);
-      setTimeout(() => {
-        setCurrentIndex(currentIndex + 1);
-        setIsTransitioning(false);
-      }, 700); // Match the CSS transition duration
+      setCurrentIndex(currentIndex + 1);
     }
   };
 
   const handlePrev = () => {
     if (currentIndex > 0) {
-      setIsTransitioning(true);
-      setTimeout(() => {
-        setCurrentIndex(currentIndex - 1);
-        setIsTransitioning(false);
-      }, 700); // Match the CSS transition duration
+      setCurrentIndex(currentIndex - 1);
     }
   };
 
@@ -53,7 +65,12 @@ const Swiper = ({ images }: { images: Array<string> }) => {
     }
   };
 
-  console.log({ currentIndex, images, isTransitioning });
+  console.log({
+    currentIndex,
+    images,
+    isTransitioning,
+    previous: images.length,
+  });
 
   return (
     <div
@@ -72,17 +89,20 @@ const Swiper = ({ images }: { images: Array<string> }) => {
             translate: `${-20 * currentIndex}%`,
           }}
         >
-          {images.map((image, index) => (
-            <div className="container">
-              <img
-                key={index}
-                src={image}
-                className={`image `}
-                alt={`Image ${index}`}
-                draggable={false}
-              />
-            </div>
-          ))}
+          {images.map((image, index) => {
+            // console.log({ currentIndexCopy });
+            return (
+              <div className={`container image-${index - currentIndex}  `}>
+                <img
+                  key={index}
+                  src={image}
+                  className={`image `}
+                  alt={`Image ${index}`}
+                  draggable={false}
+                />
+              </div>
+            );
+          })}
         </div>
       </div>
       <button
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,20 @@ import { useFetchProducts } from "./hooks/useFetchProducts";
 export default function Home() {
   const { products, images, error } = useFetchProducts();
 
+  const imgs = [
+    "/img1.jpg",
+    "/img2.jpg",
+    "/img3.avif",
+    "/img4.avif",
+    // "/img5.avif",
+    "/img6.avif",
+    "/img7.avif",
+    "/img8.jpg",
+  ];
+
   if (error) {
     return <div>{error}</div>;
   }
 
-  return <Swiper images={images} />;
+  return <Swiper images={imgs} />;
 }
